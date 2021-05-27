ad08b2593b5058aae8af007d5fa0d40170d42a1e
Cansu0 <cansuilhan97@gmail.com>
2021-05-27 19:13:56 +0200

    Enemy Spawn

--- Unity Task 2/Assets/Scripts/Spawner.cs
+++ Unity Task 2/Assets/Scripts/Spawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -9,24 +10,50 @@ public class Spawner : MonoBehaviour
     float randomX;
     Vector2 whereToSpawn;
 
-    public float spawnRate =2f;
+    public float spawnRate = 2f;
+
     float nextSpawn = 0.0f;
+    bool triggered = false; 
+
     // Start is called before the first frame update
     void Start()
     {
-       
-        
+
+
+
     }
 
     // Update is called once per frame
-    void Update()
+   private void FixedUpdate()
     {
-        if (Time.time > nextSpawn)
+        if (triggered)
         {
-            nextSpawn = Time.time + spawnRate;
-            randomX = Random.Range(41.0f, 52.0f);
-            whereToSpawn = new Vector2(randomX, transform.position.y);
-            Instantiate(enemy, whereToSpawn, Quaternion.identity);
-        }        
+
+            if (Time.time > nextSpawn)
+            {
+
+                //statt mit time.time evtl wenn finalBoss dead + spawnRate
+                nextSpawn = Time.time + spawnRate;
+                randomX = Random.Range(41.0f, 45.0f);
+                whereToSpawn = new Vector2(randomX, transform.position.y);
+                Instantiate(enemy, whereToSpawn, Quaternion.identity);
+            }
+        }
     }
+        
+       
+    
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      
+        if (other.CompareTag("Spawner")) ;
+        triggered = true;
+    }
+    
+   
+   
+   
+   
+
 }
