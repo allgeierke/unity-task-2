37fb0a1c43356ac8caf9f7e8886f03d37e65fa05
Dennis <dennisbroecker@t-online.de>
2021-05-29 15:38:06 +0200

    Fixed collider, added highscore persistence

--- Unity Task 2/Assets/Scripts/Collect.cs
+++ Unity Task 2/Assets/Scripts/Collect.cs
@@ -6,11 +6,15 @@ public class Collect : MonoBehaviour
 {
     private int coinCounter;
     public Text scoreText;
+    public Text highscoreText;
     
     // Start is called before the first frame update
     void Start()
     {
         coinCounter = 0;
+        
+        //load highscore
+        highscoreText.text = "Highscore: " + PlayerPrefs.GetInt("Highscore");
     }
 
     // Update is called once per frame
@@ -27,6 +31,12 @@ public class Collect : MonoBehaviour
             Destroy(other.gameObject);
             scoreText.text = "Score: " + coinCounter;
             Debug.Log("Coin counter: " + coinCounter);
+
+            if (coinCounter > PlayerPrefs.GetInt("Highscore"))
+            {
+                PlayerPrefs.SetInt("Highscore", coinCounter);
+                highscoreText.text = "Highscore: " + PlayerPrefs.GetInt("Highscore");
+            }
 
         }
     }
