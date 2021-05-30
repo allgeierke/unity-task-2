033937d92bfc0f4259d600a1912065b48dd0b4d7
lukasGlas <LukasGls@outlook.de>
2021-05-30 22:29:22 +0200

    laaaast fix i swear it guys

--- Unity Task 2/Assets/Scripts/EnemyControl.cs
+++ Unity Task 2/Assets/Scripts/EnemyControl.cs
@@ -90,7 +90,7 @@ public class EnemyControl : MonoBehaviour
         enemyActing = false;
         body.gravityScale = 0;
         body.constraints = RigidbodyConstraints2D.None;
-        body.velocity = new Vector2(body.velocity.x, body.velocity.y-0.2f);
+        body.velocity = new Vector2(body.velocity.x, body.velocity.y-0.5f);
         collider.isTrigger = false;
         enemySprite.color = Color.red;
         this.enabled = false;
